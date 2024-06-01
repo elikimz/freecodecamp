a7dd9a1aa027dc927c9f172378a59ff71fce8c3f
elijah -kimani <elijahkimani1293@gmail.com>
2024-06-01 22:45:41 +0300

    first commit

---
+++ callbacks.js
@@ -0,0 +1,40 @@
+// let stocks={
+//     Fruits:["bananas","mangoes","ovacado","pineaples","apple"],
+//     Liquid:["ice","water"],
+//     Holder:["cup","mug"],
+//     toppings:["chocolate","peanuts"]
+
+// };
+
+// let ice=(fruit_name, start_production)=>{
+//    setTimeout(()=>{
+//     console.log(`${stocks.Fruits[fruit_name]} was selected`)
+    
+//     start_production()
+//    },2000)
+// };
+// let order=()=>{
+//     setTimeout(()=>{
+//         console.log("production has started!")
+//         setTimeout(()=>{
+//             console.log("The food has been chopped")
+//             setTimeout(()=>{
+//                     console.log(`${stocks.Liquid[0]} and ${stocks.Liquid[1]} was selected`)
+//                     setTimeout(()=>{
+//                         console.log("The machine has been started")
+//                         setTimeout(()=>{
+//                             console.log(`${stocks.Holder[0]} was selected`)
+//                             setTimeout(()=>{
+//                                 console.log(`${stocks.toppings[0]} wa added as toppings`)
+//                                 setTimeout(()=>{
+//                                     console.log("Serve ice cream")
+//                                 },3000)
+//                             },3000)
+//                         },2000)
+//                     },1000)
+//             },1000)
+//         },2000)
+//     },1000)
+   
+// }
+// ice(1,order);
---
+++ promises.js
@@ -0,0 +1,49 @@
+let stocks={
+    Fruits:["bananas","mangoes","ovacado","pineaples","apple"],
+    Liquid:["ice","water"],
+    Holder:["cup","mug"],
+    toppings:["chocolate","peanuts"]
+
+};
+let is_shop_open=true;
+let order=(time,work)=>{
+    return new Promise ((resolve,reject)=>{
+        if(is_shop_open){
+            setTimeout(()=>{ resolve(work())},time)
+           
+        }
+        else{
+            reject(console.log("our shop is closed"))
+        }
+    })
+
+};
+order(2000,()=>console.log(`${stocks.Fruits[4]} was selected`))
+
+.then(()=>{
+    return order(1000,()=>console.log("productio has started"))
+})
+.then(()=>{
+    return order(2000,()=>console.log("The fruit was chopped"))
+})
+.then(()=>{
+    return order(1000,()=>console.log(`${stocks.Liquid[0]} and ${stocks.Liquid[1]} was selected`))
+})
+.then(()=>{
+    return order(1000,()=>console.log("Start the machine"))
+})
+.then(()=>{
+    return order(2000,()=>console.log(` ice cream was put in a ${stocks.Holder[0]}`))
+})
+.then(()=>{
+    return order(3000,()=>console.log(`${stocks.toppings[0]} was selected`))
+})
+.then(()=>{
+    return order(2000,()=>console.log("Serve the ice cream"))
+})
+.catch(()=>{
+    console.log("Customer left")
+})
+.finally(()=>{
+    console.log(" day ended,the shop is closed")
+})
